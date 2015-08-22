9b1db5fdd43deb26515a8b650681c12ef2e52061
amirouche <amirouche.boubekki@gmail.com>
2015-08-22 17:29:37 +0200

    ok with bsddb3

--- tests.py
+++ tests.py
@@ -64,153 +64,105 @@ class TestGraphDatabase(DatabaseTestCase):
 
     def test_create_vertex(self):
 
-        with self.graph.transaction():
-            v = self.graph.vertex(label='test')
+        v = self.graph.vertex(label='test')
         self.assertTrue(v)
 
     def test_create_and_get_vertex(self):
 
-        with self.graph.transaction():
-            v = self.graph.vertex(label='test')
+        v = self.graph.vertex(label='test')
 
-        with self.graph.transaction():
-            v = self.graph.get(v.uid)
+        v = self.graph.get(v.uid)
 
-        self.assertTrue(v.properties['label'], 'test')
+        self.assertTrue(v['label'], 'test')
 
     def test_create_modify_and_get_vertex(self):
-        with self.graph.transaction():
-            v = self.graph.vertex(label='test')
-            v.properties['value'] = 'key'
-            v.save()
+        v = self.graph.vertex(label='test')
+        v['value'] = 'key'
+        v.save()
 
-        with self.graph.transaction():
-            v = self.graph.get(v.uid)
+        v = self.graph.get(v.uid)
 
-        self.assertTrue(v.properties['value'] == 'key')
+        self.assertTrue(v['value'] == 'key')
 
     def test_create_and_get_modify_and_get_again_vertex(self):
 
-        with self.graph.transaction():
-            v = self.graph.vertex(label='test')
+        v = self.graph.vertex(label='test')
 
-        with self.graph.transaction():
-            v = self.graph.get(v.uid)
-            v.properties['value'] = 'key'
-            v.save()
+        v = self.graph.get(v.uid)
+        v['value'] = 'key'
+        v.save()
 
-        with self.graph.transaction():
-            v = self.graph.get(v.uid)
+        v = self.graph.get(v.uid)
 
-        self.assertTrue(v.properties['value'] == 'key')
+        self.assertTrue(v['value'] == 'key')
 
     def test_create_modify_and_get_edge(self):
 
-        with self.graph.transaction():
-            start = self.graph.vertex(label='test')
-            end = self.graph.vertex(label='test')
-            edge = self.graph.edge(start, end)
-            edge.properties['hello'] = 'world'
-            edge.save()
+        start = self.graph.vertex(label='test')
+        end = self.graph.vertex(label='test')
+        edge = self.graph.edge(start, end)
+        edge['hello'] = 'world'
+        edge.save()
 
-        with self.graph.transaction():
-            edge = self.graph.get(edge.uid)
+        edge = self.graph.get(edge.uid)
 
-        self.assertTrue(edge.properties['hello'] == 'world')
+        self.assertTrue(edge['hello'] == 'world')
 
     def test_create_edge_and_checkout_vertex(self):
 
-        with self.graph.transaction():
-            start = self.graph.vertex(label='start')
-            end = self.graph.vertex(label='end')
-            self.graph.edge(start, end)
+        start = self.graph.vertex(label='start')
+        end = self.graph.vertex(label='end')
+        self.graph.edge(start, end)
 
         # retrieve start and end
-        with self.graph.transaction():
-            start = self.graph.get(start.uid)
-            end = self.graph.get(end.uid)
-            self.assertTrue(list(start.outgoings()) and list(end.incomings()))
+        start = self.graph.get(start.uid)
+        end = self.graph.get(end.uid)
+        self.assertTrue(list(start.outgoings()) and list(end.incomings()))
 
     def test_delete_vertex(self):
 
-        with self.graph.transaction():
-            v = self.graph.vertex(label='delete-node')
-            v.properties['value'] = 'key'
-            v.save()
+        v = self.graph.vertex(label='delete-node')
+        v['value'] = 'key'
+        v.save()
 
         # retrieve it
-        with self.graph.transaction():
-            self.graph.get(v.uid)
+        self.graph.get(v.uid)
 
-        with self.graph.transaction():
-            v.delete()
+        v.delete()
 
-        with self.graph.transaction():
-            self.assertRaises(AjguDBException, self.graph.get, v.uid)
+        self.assertRaises(AjguDBException, self.graph.get, v.uid)
 
     def test_delete_edge(self):
 
-        with self.graph.transaction():
-            # vertices
-            start = self.graph.vertex(label='start')
-            end = self.graph.vertex(label='end')
-            # first edge
-            edge = self.graph.edge(start, end)
+        # vertices
+        start = self.graph.vertex(label='start')
+        end = self.graph.vertex(label='end')
+        # first edge
+        edge = self.graph.edge(start, end)
 
         # delete edge
-        with self.graph.transaction():
-            edge = self.graph.get(edge.uid)
-            edge.delete()
+        edge = self.graph.get(edge.uid)
+        edge.delete()
 
-        with self.graph.transaction():
-            self.assertRaises(AjguDBException, self.graph.get, edge.uid)
+        self.assertRaises(AjguDBException, self.graph.get, edge.uid)
 
         # check end and start vertex are up-to-date
-        with self.graph.transaction():
-            start = self.graph.get(start.uid)
-            self.assertEquals(len(list(start.outgoings())), 0)
-            end = self.graph.get(end.uid)
-            self.assertEquals(len(list(end.incomings())), 0)
+        start = self.graph.get(start.uid)
+        self.assertEquals(len(list(start.outgoings())), 0)
+        end = self.graph.get(end.uid)
+        self.assertEquals(len(list(end.incomings())), 0)
 
 
+class TestGraphFiltering(DatabaseTestCase):
 
-class TestUzelmumuQuery(DatabaseTestCase):
+    def test_simple_filter(self):
+        self.graph.vertex(label='test', foo='bar')
+        self.graph.vertex(label='test', foo='bar')
+        self.graph.vertex(label='another', foo='bar')
+        self.assertEqual(self.graph.filter(label='test').count(), 2)
 
-    def test_run_uzelmumu_query(self):
-        # populate database
-        database = AjguDB('/tmp/ajgudb')
-
-        # create user
-        with database.transaction():
-            amirouche = self.graph.vertex('user')
-            amirouche.properties['name'] = 'amirouche'
-
-        # create feed
-        with database.transaction():
-            amirouche = self.graph.get(amirouche.uid)
-
-            hypermove = self.graph.vertex('feed')
-            hypermove.properties['url'] = 'http://hyperdev.fr/atom.xml'
-            self.graph.edge(amirouche, 'feeds', hypermove)
-
-            hypermove = self.graph.vertex('feed')
-            url = 'http://www.hyperdev.fr/keyword/fran%C3%A7ais/atom.xml'
-            hypermove.properties['url'] = url
-            self.graph.edge(amirouche, 'feeds', hypermove)
-
-        query = graph().get(amirouche.uid).outgoings('feeds')
-        query = query.end().freeze()
-        query = query.serialize()
-        query = loads(dumps(query))
-
-        feeds = run(database, query)
-
-        urls = list()
-        for feed in feeds:
-            urls.append(feed['properties']['url'])
-        if not (urls[0] == 'http://hyperdev.fr/atom.xml'):
-            return False
-        url = 'http://www.hyperdev.fr/keyword/fran%C3%A7ais/atom.xml'
-        if not (urls[1] == url):
-            return False
-        return True
+    def test_two_filters(self):
+        self.graph.vertex(label='test', value=1, foo='bar')
+        self.graph.vertex(label='test', value=2, foo='bar')
+        self.graph.vertex(label='another', value=3, foo='bar')
+        self.assertEqual(self.graph.filter(label='test', value=1).count(), 1)

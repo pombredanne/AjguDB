02e0b3590f213a61b8c18b19c3df7f3749b9b02e
amirouche <amirouche.boubekki@gmail.com>
2015-08-26 19:37:35 +0200

    ajgudb: add one to retrieve only one vertex or edge. Returns None otherwise.

--- ajgudb/ajgudb.py
+++ ajgudb/ajgudb.py
@@ -153,5 +153,10 @@ class AjguDB(object):
         from gremlin import query
         return lambda iterator=None: query(*steps)(self, iterator)
 
-    def one(self):
-        raise NotImplementedError
+    def one(self, properties):
+        try:
+            uid = next(self.select(**properties)).value
+        except StopIteration:
+            return None
+        else:
+            return self.get(uid)

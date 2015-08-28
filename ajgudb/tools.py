5470b0fe71c12969ef0fc665de0b66d965064405
amirouche <amirouche.boubekki@gmail.com>
2015-08-28 15:44:29 +0200

    ajgudb.tools: add graph-tool conversion utility

--- ajgudb/tools.py
+++ ajgudb/tools.py
@@ -1,7 +1,12 @@
-import networkx as nx
+from .gremlin import vertices
+from .gremlin import edges
+from .gremlin import get
 
 
 def to_nx(db):
+    """Convert db to networkx representation"""
+    import networkx as nx
+
     graph = nx.DiGraph()
 
     graph.add_nodes_from(db.vertices())
@@ -12,3 +17,27 @@ def to_nx(db):
         graph.add_edge(start, end)
 
     return graph
+
+
+def to_gt(db):
+    """Convert db to graph-tool representation"""
+    from graph_tool.all import Graph
+
+    graph = Graph(directed=True)
+
+    mapping = dict()
+
+    for native in db.query(vertices, get)():
+        vertex = graph.add_vertex()
+        mapping[native.uid] = graph.vertex_index[vertex]
+
+    for native in db.query(edges, get)():
+        start = native.start().uid
+        start = mapping[start]
+        end = native.end().uid
+        end = mapping[end]
+        graph.add_edge(start, end)
+
+    return graph
+
+
